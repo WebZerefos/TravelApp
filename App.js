1e2e7a3f7333e49276c0e2e22ba9dc10ebd0e2d4
WebZerefos <anything.codable@gmail.com>
2023-01-09 22:47:12 -0300

    Navigation Setup Done

--- App.js
+++ App.js
@@ -1,8 +1,23 @@
+import 'react-native-gesture-handler';
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import Home from './src/creens/Home';
+import AttractionDetails from './src/creens/AttractionDetails';
+import Gallery from './src/creens/Gallery';
+
+const Stack = createStackNavigator();
 
 const App = () => {
-  return <Home />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Attractiondetails" component={AttractionDetails} />
+        <Stack.Screen name="Gallery" component={Gallery} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
