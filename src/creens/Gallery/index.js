549f5c271fb80b8b97bc4e510e793a9fc240ff26
WebZerefos <anything.codable@gmail.com>
2023-01-11 10:01:47 -0300

    Gallery screen done

--- src/creens/Gallery/index.js
+++ src/creens/Gallery/index.js
@@ -1,10 +1,38 @@
-import {Text, SafeAreaView} from 'react-native';
+import {
+  SafeAreaView,
+  Image,
+  FlatList,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React, {memo} from 'react';
+import styles from './styles';
 
-const Gallery = () => {
+const Gallery = ({route, navigation}) => {
+  const {images} = route?.params || {};
+
+  const goBack = () => {
+    navigation.goBack();
+  };
+
   return (
-    <SafeAreaView>
-      <Text>Gallery</Text>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <FlatList
+          style={{marginBottom: 15, paddingHorizontal: 15}}
+          data={images}
+          renderItem={({item}) => (
+            <Image style={styles.image} source={{uri: item}} />
+          )}
+        />
+
+        <TouchableOpacity style={styles.backContainer} onPress={goBack}>
+          <Image
+            style={styles.icon}
+            source={require('../../assets/back.png')}
+          />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
---
+++ src/creens/Gallery/styles.js
@@ -0,0 +1,24 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    width: '100%',
+    height: 400,
+    borderRadius: 20,
+    marginTop: 15,
+    position: 'relative',
+  },
+  backContainer: {
+    position: 'absolute',
+    margin: 20,
+  },
+  icon: {
+    width: 40,
+    height: 40,
+  },
+});
+
+export default styles;
